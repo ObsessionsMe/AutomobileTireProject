77150f20aeaa34af315eda7d9b66e4c93621d7cf
xiapeng <1448525265@qq.com>
2020-08-12 21:44:31 +0800

    。。。。。

--- ReptileCarPlatformData/ReptileCarPlatformData/Program.cs
+++ ReptileCarPlatformData/ReptileCarPlatformData/Program.cs
@@ -15,7 +15,7 @@ namespace ReptileCarPlatformData
         static void Main(string[] args)
         {
             Console.WriteLine("开始抓取数据中....");
-            //1获取三个筛选条件的组合情况，得到集合searchCondition 例如:ml?make=acura&model=&year=2001，sm?make=acura&model=cl&year=2001
+            //1获取三个筛选条件的组合情况，得到集合searchCondition 例如:[make=acura&model=&year=2001，make=acura&model=cl&year=2001]
             //var urlList = getAllCondition();
 
             //2发起请求获取数据，异常时重新发起请求，得到数据
@@ -38,13 +38,23 @@ namespace ReptileCarPlatformData
         private static List<string> getAllCondition()
         {
             var level1Result = ApiRequest.RequestAutomobileBrandData();
-            var  level1List = JArray.Parse(level1Result.ToString());
-            //遍历一级条件的数据--171条
+            List<conditionEntity> level1List = JsonConvert.DeserializeObject<List<conditionEntity>>(level1Result.ToString());
+            string yearParam = "";
+            //1遍历品牌(1级)的数据--171条
             foreach (var item in level1List)  //查找某个字段与值
             {
-                string model = item["name"].ToString();//小写
-                string make = item["slug"].ToString();//大写
+                yearParam = "yr?make=" + item .slug+ "&model=&year=";
+                //2通过品牌查询出年份数据
+               var yearResult =  ApiRequest.RequestData(yearParam);
+                var level2List = JsonConvert.DeserializeObject<List<conditionEntity>>(yearResult.ToString());
+                foreach (var item2 in level2List)
+                {
+                    //3品牌和年份查询出型号的数据
+                    var typeResult = ApiRequest.RequestData(yearParam);
+                    var level3List = JsonConvert.DeserializeObject<List<conditionEntity>>(typeResult.ToString());
+                    //4
 
+                }
             }
             return null;
         }
