e654a6748391bb9a46c8c2b8eec2820de34fa2a7
86159 <86159@DESKTOP-EONNA4V>
2020-08-12 18:42:59 +0800

    获取数据

--- ReptileCarPlatformData/ReptileCarPlatformData/Program.cs
+++ ReptileCarPlatformData/ReptileCarPlatformData/Program.cs
@@ -1,4 +1,7 @@
-﻿using ReptileCarPlatformData.Tool;
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using ReptileCarPlatformData.Model;
+using ReptileCarPlatformData.Tool;
 using System;
 using System.Collections.Generic;
 
@@ -7,18 +10,25 @@ namespace ReptileCarPlatformData
     class Program
     {
 
+        public static string TestDataParams = "make=audi&model=a6&year=2018";
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
-            Console.ReadLine();
+            Console.WriteLine("开始抓取数据中....");
             //1获取三个筛选条件的组合情况，得到集合searchCondition 例如:ml?make=acura&model=&year=2001，sm?make=acura&model=cl&year=2001
+            //var urlList = getAllCondition();
 
-            //2发起请求获取数据，异常时重新发起请求，将得到的数据存储起来/或者入库
-            var conditionList = getAllCondition();
-            foreach (var item in conditionList)
-            {
-                ApiRequest.RequestByGet(item);
-            }
+            //2发起请求获取数据，异常时重新发起请求，得到数据
+            var dataResult = ApiRequest.RequestData(TestDataParams);
+            List<requestDataEntity> list = JsonConvert.DeserializeObject<List<requestDataEntity>>(dataResult.ToString());
+            Console.WriteLine(list);
+
+            //foreach (var item in conditionList)
+            //{
+            //    ApiRequest.RequestByGet(item);
+            //}
+
+            //3入库
         }
 
         /// <summary>
@@ -27,8 +37,16 @@ namespace ReptileCarPlatformData
         /// <returns></returns>
         private static List<string> getAllCondition()
         {
+            var level1Result = ApiRequest.RequestAutomobileBrandData();
+            var  level1List = JArray.Parse(level1Result.ToString());
+            //遍历一级条件的数据--171条
+            foreach (var item in level1List)  //查找某个字段与值
+            {
+                string model = item["name"].ToString();//小写
+                string make = item["slug"].ToString();//大写
+
+            }
             return null;
         }
-
     }
 }
--- ReptileCarPlatformData/ReptileCarPlatformData/Tool/ApiRequest.cs
+++ ReptileCarPlatformData/ReptileCarPlatformData/Tool/ApiRequest.cs
@@ -1,8 +1,9 @@
 ﻿using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
-using ReptileCarPlatformData.Model;
 using System;
 using System.Collections.Generic;
+using System.IO;
+using System.Net;
 using System.Net.Http;
 using System.Text;
 
@@ -15,30 +16,82 @@ namespace ReptileCarPlatformData.Tool
     public class ApiRequest
     {
         //1接口请求地址
-        public static string ApiUrl = "https://services.wheel-size.com/widget/finder/api/";
+        public static string ApiUrl = "https://services.wheel-size.com/widget/finder/api/sm?";
 
+        //2所有品牌默认请求地址
+        public static string automobileBrandApi = "https://services.wheel-size.com/widget/finder/api/mk?make=&model=&year=";
+
+        //条件查询请求地址
+        public static string ConditionApiUrl = "https://services.wheel-size.com/widget/finder/api/mk?";
 
         /// <summary>
-        ///  发起Get请求，获取查询搜索的结果集合
+        ///  发起Get请求，获取所有品牌的结果集合
         /// </summary>
         /// <param name="param"></param>
         /// <returns></returns>
-        public static object RequestByGet(string param)
+        public static object RequestAutomobileBrandData()
         {
             object result = null;
-            using (var client = new HttpClient())
+            try
             {
-                var responseString = client.GetStringAsync(ApiUrl + param);
-                if (responseString == null)
+                HttpWebRequest req = (HttpWebRequest)WebRequest.Create(automobileBrandApi);
+                RequestByGet(req);
+                HttpWebResponse resp = (HttpWebResponse)req.GetResponse();
+                Stream stream = resp.GetResponseStream();
+                using (StreamReader reader = new StreamReader(stream, Encoding.Default))
                 {
-                    //请求失败
-                    return result;
+                    result = reader.ReadToEnd();
                 }
-                result = responseString;
-                //解析数据
-                //result = JsonConvert.DeserializeObject<List<requestDataEntity>>(responseString.Result);
+            }
+            catch (Exception ex)
+            {
+                throw ex;
             }
             return result;
+        }
+
+        /// <summary>
+        /// 发起Get请求，获取返回结果集合
+        /// </summary>
+        /// <param name="param"></param>
+        /// <returns></returns>
+        public static object RequestData(string param)
+        {
+            object result = null;
+            try
+            {
+                HttpWebRequest req = (HttpWebRequest)WebRequest.Create(ApiUrl+ param);
+                RequestByGet(req);
+                HttpWebResponse resp = (HttpWebResponse)req.GetResponse();
+                Stream stream = resp.GetResponseStream();
+                using (StreamReader reader = new StreamReader(stream, Encoding.Default))
+                {
+                    result = reader.ReadToEnd();
+                }
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+            return result;
+        }
+
+
+            /// <summary>
+            /// 设置请求头信息/默认
+            /// </summary>
+            /// <param name="client"></param>
+            public static void RequestByGet(HttpWebRequest req)
+        {
+            req.Method = "GET";
+            req.Host = "services.wheel-size.com";
+            req.Referer = "https://services.wheel-size.com/widget/finder/?config";
+            req.Accept = "application/json, text/plain, */*";
+            req.UserAgent = "Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/78.0.3904.108 Safari/537.36";
+            req.Headers["Accept-Language"] = "zh-CN,zh;q=0.9";
+            //req.Headers["X-CSRFToken"] = "wELoTjkA6dYLTIRu1dcx6QNFgPkXkTbtlldvokPU1cLJCsq8GHtSIuPiZh2pYhkQ";
+            req.Headers["X-Csrf-Token"] = "gbSDMgaMm8sgKWTR1TTFxAvLWldsudj9";
+            //req.Headers["Cookie"] = "__guid=90766360.1459916678094302000.1597213409162.0479; _ga=GA1.2.585568688.1597213441; _gid=GA1.2.1681335507.1597213441; csrftoken=VV0P9ui8Z1pQtWNjDQWwF15O8d80Bhl3KCsWEvNsU0cOcGmXikdRhF7rRFQsfFuq; monitor_count=4; _gat=1";
         }
     }
 }
